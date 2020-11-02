7653da12a38e186610e6bb62ffc2979f0f94c295
Shahrukh <muhammad.shahrukh@techloyce.com>
2020-11-02 12:04:49 +0500

    updating 12:04

--- data_migration/__manifest__.py
+++ data_migration/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/contact_fields.xml',
+        # 'views/contact_fields.xml',
         'views/company_fields.xml',
         'views/deals.xml'
     ],
