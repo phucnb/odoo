51e842164cb7268e8797e0c0dfb69b9540d65b04
tme247sur <straussadam55@gmail.com>
2020-11-09 13:48:41 +0500

    adding contacts and tickets views to manifest file

--- data_migration/__manifest__.py
+++ data_migration/__manifest__.py
@@ -25,10 +25,10 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        # 'views/contact_fields.xml',
+        'views/contact_fields.xml',
         'views/company_fields.xml',
         'views/deals.xml',
-        # 'views/ticket_fields.xml'
+        'views/ticket_fields.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
