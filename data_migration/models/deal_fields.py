59652c62057311b9d30e9e0bcecc7fd102dc4fa8
Shahrukh <muhammad.shahrukh@techloyce.com>
2020-11-17 12:48:28 +0500

    commit deal multiple selection fields again

--- data_migration/models/deal_fields.py
+++ data_migration/models/deal_fields.py
@@ -36,28 +36,20 @@ class DealFields(models.Model):
     hs_deal_contacts = fields.Many2many('res.partner', 'deal_contact_rel', 'partner_id', 'deal_id', 'Contacts')
     hs_deal_companies = fields.Many2one('res.partner', 'Company')
     n247s_lifecycle_stage = fields.Selection(S247_LIFECYCLE_STAGE, "247S Lifecycle Stage")
-    dealers_quoting_this_deal = fields.One2many('crm.lead_dealers_quoting_this_deal', 'partner_id', 'Dealers quoting this deal',
-                                                 help='Please add/select the dealers quoting e.g. A-Z Bus Sales, '
-                                                      'Bryson, KLC, Midwest Transit, Rush') #multiple
+    dealers_quoting_this_deal = fields.One2many('crm.lead_dealers_quoting_this_deal', 'opportunity_id',
+                                                'Dealers quoting this deal',
+                                                help='Please add/select the dealers quoting e.g. A-Z Bus Sales, '
+                                                     'Bryson, KLC, Midwest Transit, Rush')
     end_user = fields.Char("End-User")
     isr = fields.Selection(ISR, "ISR")
     lost_reason_notes = fields.Text("Lost Reason Notes")
     opportunity_number = fields.Char("Opportunity Number")
     opportunity_link = fields.Char("Opportunity Link")
-    product_s_considered = fields.Many2many('crm.lead_product_s_considered', 'partner_id', 'Products Considered',
+    product_s_considered = fields.Many2many('crm.lead_product_s_considered', 'opportunity_id', 'Products Considered',
                                             help='Please add/select the considered products e.g. ZeusPT, ZeusHD, '
                                                  'Touchdown™, GPS, G-Force, IP Cameras w/SARAH4\'s, Stop Arm Cameras,'
-                                                 'Demo, Install, TRS') #multiple
+                                                 'Demo, Install, TRS')
     # quote_link = fields.Char("Quote Link")
     # quote_number = fields.Char("Quote Number")
     sales_number = fields.Char("Sales Number")
     state = fields.Char('State/Region')
-
-
-
-
-
-
-
-
-
--- data_migration/models/dealone2manymodels.py
+++ data_migration/models/dealone2manymodels.py
@@ -4,12 +4,12 @@ from odoo import models, fields, api
 class DealersQuoting(models.Model):
     _name = 'crm.lead_dealers_quoting_this_deal'
 
-    partner_id = fields.Many2one('res.partner', 'Partner Id')
+    opportunity_id = fields.Many2one('crm.lead', 'Opportunity Id')
     name = fields.Char("Value")
 
 
 class ProductConsidered(models.Model):
     _name = 'crm.lead_product_s_considered'
 
-    partner_id = fields.Many2one('res.partner', 'Partner Id')
+    opportunity_id = fields.Many2one('crm.lead', 'Opportunity Id')
     name = fields.Char("Value")
