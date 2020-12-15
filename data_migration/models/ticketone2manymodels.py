e84e8cce3281977b3c0909b3ad2c30a93631d04b
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-15 16:55:39 -0500

    Update ticketone2manymodels.py

--- data_migration/models/ticketone2manymodels.py
+++ data_migration/models/ticketone2manymodels.py
@@ -5,3 +5,8 @@ class S247Product(models.Model):
     _name = 'helpdesk.ticket_s247_product'
 
     name = fields.Char("Value")
+
+class Category(models.Model):
+    _name = 'helpdesk.hs_ticket_category'
+
+    name = fields.Char("Value")
