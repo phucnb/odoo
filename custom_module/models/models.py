12914bf06ccc94061ea6e1ee1c8681bce5a5ea47
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-16 08:09:51 -0500

    add new fields for company card

--- custom_module/models/models.py
+++ custom_module/models/models.py
@@ -1,5 +1,68 @@
 from odoo import models, fields, api
 
+NP_SCORE = [
+    ('0', '0'),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10', '10')
+]
+
+CRR = [
+    ('demo', 'demo')
+]
+
+BUS_MFG = [
+    ('demo', 'demo')
+]
+
+BUS_TYPE = [
+    ('demo', 'demo')
+]
+
+SERVICE_PLAN_TYPE = [
+    ('1 visit', '1 visit'),
+    ('2 visit', '2 visit'),
+    ('3 visit', '3 visit')
+]
+
+CAMERA_VENDOR = [
+    ('Angletrax', 'Angletrax'),
+    ('Seon', 'Seon'),
+    ('Gatekeeper', 'Gatekeeper'),
+    ('REI', 'REI'),
+    ('Safety Vision', 'Safety Vision'),
+    ('Provision', 'Provision'),
+    ('Zentinel', 'Zentinel'),    
+    ('Other', 'Other')
+]
+
+STOP_ARM_THIRD_PARTY = [
+    ('No', 'No'),
+    ('Busgaurd', 'Busgaurd'),
+    ('ATs', 'ATs')
+]
+
+DISTRICT_TYPE = [
+    ('Rural', 'Rural'),
+    ('Urban', 'Urban')
+]
+
+NO_BELL_TIMES = [
+    ('0', '0'),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5')
+]
+
 class ResPartnerField(models.Model):
 
      _inherit = 'res.partner'
@@ -19,7 +82,19 @@ class ResPartnerField(models.Model):
      hs_email_optout = fields.Boolean('Unsubscribed from all email')
      # 1hs_deal_companies = fields.Many2one('res.partner', 'Company')
 
-  
+     247_customer = fields.Boolean('247 Customer')
+     np_score = fields.Selection(NP_SCORE, "NP Score")
+     crr = fields.Selection(CRR, 'CRR')
+     bus_mfg = fields.Selection(BUS_MFG, 'Bus MFG')
+     bus_type = fields.Selection(BUS_TYPE, 'Bus Type')
+     service_plan_type = fields.Selection(SERVICE_PLAN_TYPE, 'Service Plan Type')
+     fleet_inspection = fields.Boolean('Fleet Inspection')   
+     no_vihicles_serviced = fields.Integer('# Vehicles Serviced')
+     camera_vendor = fields.Selection(CAMERA_VENDOR, 'Camera Vendor')
+     shield_cams = fields.Boolean('Shield Cams')
+     stop_arm_third_party = fields.Selection(STOP_ARM_THIRD_PARTY, 'Stop Arm Third Party')
+     district_type = fields.Selection(DISTRICT_TYPE, 'District Type')
+     no_bell_times = fields.Selection(NO_BELL_TIMES, '# Bell Times')
 
 
     # sale_order_count1 = fields.Integer(compute='_compute_sale_order_count1', string='Sale Order Count')
