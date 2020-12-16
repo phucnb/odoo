0b2e54a2c0ca35fdeeae7636fabc70157fc04f37
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-16 08:18:21 -0500

    update company view

--- custom_module/models/models.py
+++ custom_module/models/models.py
@@ -79,10 +79,8 @@ class ResPartnerField(models.Model):
      hs_email_last_click_date = fields.Date('Last marketing email click date')
      hs_email_last_email_name = fields.Text("Last marketing email name")
      hs_marketable_until_renewal = fields.Boolean('Marketing Until Next Update')
-     hs_email_optout = fields.Boolean('Unsubscribed from all email')
-     # 1hs_deal_companies = fields.Many2one('res.partner', 'Company')
-
-     247_customer = fields.Boolean('247 Customer')
+     hs_email_optout = fields.Boolean('Unsubscribed from all email')     
+     _247_customer = fields.Boolean('247 Customer')
      np_score = fields.Selection(NP_SCORE, "NP Score")
      crr = fields.Selection(CRR, 'CRR')
      bus_mfg = fields.Selection(BUS_MFG, 'Bus MFG')
@@ -96,24 +94,3 @@ class ResPartnerField(models.Model):
      district_type = fields.Selection(DISTRICT_TYPE, 'District Type')
      no_bell_times = fields.Selection(NO_BELL_TIMES, '# Bell Times')
 
-
-    # sale_order_count1 = fields.Integer(compute='_compute_sale_order_count1', string='Sale Order Count')
-   
-    # def _compute_sale_order_count1(self):
-    #     # retrieve all children partners and prefetch 'parent_id' on them
-    #     all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
-    #     all_partners.read(['parent_id'])
-
-    #     sale_order_groups = self.env['sale.order'].read_group(
-    #         domain=[('partner_id', 'in', all_partners.ids)],
-    #         fields=['partner_id'], groupby=['partner_id']
-    #     )
-    #     partners = self.browse()
-    #     for group in sale_order_groups:
-    #         partner = self.browse(group['partner_id'][0])
-    #         while partner:
-    #             if partner in self:
-    #                 partner.sale_order_count1 += group['partner_id_count']
-    #                 partners |= partner
-    #             partner = partner.parent_id
-    #     (self - partners).sale_order_count1 = 0
