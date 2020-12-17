9bc834ac2d4b9a333f4ef598df5ce9c0cdf8aa2d
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-17 07:47:24 -0500

    update company views

--- custom_module/models/models.py
+++ custom_module/models/models.py
@@ -26,11 +26,6 @@ BUS_TYPE = [
     ('demo', 'demo')
 ]
 
-SERVICE_PLAN_TYPE = [
-    ('1 visit', '1 visit'),
-    ('2 visit', '2 visit'),
-    ('3 visit', '3 visit')
-]
 
 CAMERA_VENDOR = [
     ('Angletrax', 'Angletrax'),
@@ -63,6 +58,37 @@ NO_BELL_TIMES = [
     ('5', '5')
 ]
 
+SERVER_TYPE = [
+    ('LAPTOP', 'Laptop'),
+    ('BLADE SERVER', 'Blade Server'),
+    ('ML310EG8', 'HP ProLiant ML310e Gen8'),
+    ('ML30G9', 'HP ProLiant ML30 Gen9'),
+    ('ML30G10', 'HP Proliant ML30 Gen10'),
+    ('ML110G10', 'HP Prolant ML110 Gen10'),
+    ('WXPLGV2', 's247 TD Control Center'),
+]
+ 
+WIRELESS_SPEED = [
+    ('24GHZ', '2.4GHz'),
+    ('5GHZ', '5GHz'),
+    ('MIXED', 'Mixed')
+]
+ 
+WAP_TYPE = [
+    ('OMNI', 'Omnidirectional'),
+    ('DIRECT_NON_NANO', 'Directional Antenna w/ standard AP'),
+    ('NANO', 'Nanobeam'),
+    ('MIXED', 'Mixed'),
+]
+ 
+SERVICE_PLAN_TYPE = [
+    ('THREE PHASE', 'Three Phase'),
+    ('TWO PHASE', 'Two Phase'),
+    ('ONE PHASE', 'One Phase'),
+    ('FLEET INSPECTION', 'Fleet Inspection'),
+]
+
+
 class ResPartnerField(models.Model):
 
      _inherit = 'res.partner'
@@ -89,6 +115,7 @@ class ResPartnerField(models.Model):
      fleet_inspection = fields.Boolean('Fleet Inspection')   
      no_vehicles_serviced = fields.Integer('# Vehicles Serviced')
      camera_vendor = fields.Selection(CAMERA_VENDOR, 'Camera Vendor')
+     camera_vendor_specify = fields.Char("Specify Vendor")
      shield_cams = fields.Boolean('Shield Cams')
      stop_arm_third_party = fields.Selection(STOP_ARM_THIRD_PARTY, 'Stop Arm Third Party')
      district_type = fields.Selection(DISTRICT_TYPE, 'District Type')
@@ -116,5 +143,41 @@ class ResPartnerField(models.Model):
             readonly=True, store=True, index=True
      )
 
+      # ***CAMERA TAB***
+     camera_vendor = fields.Selection(OPTIONS2, "Camera Vendor")
+     camera_vendor_other = fields.Boolean("Other")
+     camera_vendor_specify = fields.Char("Specify Vendor")
+     camera_number = fields.Integer("# of Cameras")
+     camera_number_per_bus = fields.Integer("# of Cameras / Bus")
+     camera_type_shield = fields.Boolean("Shield Cam")
+     camera_type_sac = fields.Boolean("Stop Arm Camera")
+     camera_type_sac_3rd_pt = fields.Boolean("SAC - 3rd Party")
+     camera_type_sac_3rd_pt_vendor = fields.Selection(OPTIONS2, "3rd Party Vendor")
+     camera_type_sac_3rd_pt_specify = fields.Char("Specify Vendor")
+     camera_vendor_pref = fields.Selection(OPTIONS2, "Preferred Vendor")
+     camera_vendor_pref_specify = fields.Char("Specify Vendor")
+
+    # ***SERVICE TAB***
+     service_plan = fields.Boolean("Service Plan")
+     service_plan_type = fields.Selection(SERVICE_PLAN_TYPE, "Agreement Type")
+     service_vehicle_num = fields.Integer("Number of Vehicles Serviced")
+     service_po_received = fields.Date("Service Order Signed")
+
+      # ***WIRELESS TAB***
+     wireless_check = fields.Boolean("Wireless")
+     wireless_platform = fields.Selection(WIRELESS_PLATFORM, "Platform")
+     touchdown_type = fields.Selection(TOUCHDOWN_TYPE, "Touchdown Type")
+     mss_check = fields.Boolean("MSS")
+     mss_count = fields.Integer("MSS Count")
+     server_type = fields.Selection(SERVER_TYPE, "Server Type")
+     wireless_speed = fields.Selection(WIRELESS_SPEED, "Wireless Speed")
+     wireless_ap_type = fields.Selection(WAP_TYPE, "AP Type")
+     wireless_ap_count = fields.Integer("AP Count")
+     wireless_vehicle_count = fields.Integer("Vehicle Count")
+     tv_access = fields.Boolean("TeamViewer")
+     tv_id = fields.Char("TeamViewer ID")
+     tv_version = fields.Char("TeamViewer Version") 
+     tv_monitor = fields.Boolean("TeamViewer Monitor")
+     server_inst_date = fields.Date("Server Install Date")
 
 
