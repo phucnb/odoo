ede2252a01f43dc64b5201fbf1a99e4896313c71
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-22 16:29:37 -0500

    add relational field

--- custom_module/models/helpdesk.py
+++ custom_module/models/helpdesk.py
@@ -4,3 +4,18 @@ class TicketFields(models.Model):
     _inherit = 'helpdesk.ticket'
 
     multiple_product = fields.Boolean("Multiple Product")
+    child_ids = fields.One2many(
+        'res.partner', 'parent_id',
+        string='Contact',
+        domain=[('active', '=', True)]
+     )  # force "active_test" domain to bypass _search() override
+     
+     customer_main = fields.Many2one(
+            'res.partner', string='Customer'
+     )
+        
+     customer_busaddress = fields.Char(
+            string='Bus Garage Address',
+            related='customer_main.bus_garage_address', related_sudo=True, compute_sudo=True,
+            readonly=True, store=True, index=True
+     )
