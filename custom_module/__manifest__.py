2e52da6527f0eb758351f6fdcd085aa43ed2b8ac
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-22 13:58:18 -0500

    Update __manifest__.py

--- custom_module/__manifest__.py
+++ custom_module/__manifest__.py
@@ -19,12 +19,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','contacts','data_migration', 'sale_management', 'crm'],
+    'depends': ['base','contacts','data_migration', 'sale_management', 'crm','helpdesk'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/helpdesk.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
