176a8fda17368d642d0217799d2f0b099afaff17
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-30 12:43:53 -0500

    Update selection_config.py

--- custom_module/models/selection_config.py
+++ custom_module/models/selection_config.py
@@ -2,21 +2,21 @@ from odoo import fields, models
 
 
 class SelectionChild(models.Model):
-    _name = 'selection.child'
+    _name = 'selection.main'
     _description = 'Module help manager selection B'
 
     name = fields.Char(string='Name')
 
 
 class SelectionParent(models.Model):
-    _name = 'selection.parent'
+    _name = 'selection.type'
     _description = 'Module help manager selection A'
 
     name = fields.Char(string='Name')
-    child_ids = fields.One2many('selection.child', 'parent_id', string='Child IDs')
+    child_ids = fields.One2many('selection.main', 'parent_id', string='Child IDs')
 
 
 class SelectionChildInherit(models.Model):
-    _inherit = 'selection.child'
+    _inherit = 'selection.main'
 
-    parent_id = fields.Many2one('selection.parent', string='Parent')
+    parent_id = fields.Many2one('selection.type', string='Parent')
