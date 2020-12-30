ead2c8161c5d7170d2bb62bab03be51051ffd49f
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-30 13:56:46 -0500

    resolution

--- custom_module/models/selection_config.py
+++ custom_module/models/selection_config.py
@@ -15,6 +15,19 @@ class SelectionParent(models.Model):
     name = fields.Char(string='Name')
     child_ids = fields.One2many('selection.main', 'parent_id', string='Child IDs')
 
+class SelectionParent3(models.Model):
+    _name = 'selection.type'
+    _description = 'Module help manager selection A'
+
+    name = fields.Char(string='Name')
+    child_ids = fields.One2many('selection.resolution', 'parent_id', string='Child IDs')
+
+class SelectionParent2(models.Model):
+    _name = 'selection.type'
+    _description = 'Module help manager selection A'
+
+    name = fields.Char(string='Name')
+    child_ids = fields.One2many('selection.issue', 'parent_id', string='Child IDs')
 
 class SelectionChildInherit(models.Model):
     _inherit = 'selection.main'
@@ -29,5 +42,16 @@ class SelectionIssue(models.Model):
 
 class SelectionIssueInherit(models.Model):
     _inherit = 'selection.issue'
+
+    parent_id = fields.Many2one('selection.type', string='Parent')
+
+class SelectionResolution(models.Model):
+    _name = 'selection.resolution'
+    _description = 'Module help manager selection B'
+
+    name = fields.Char(string='Name')
+
+class SelectionResolutionInherit(models.Model):
+    _inherit = 'selection.resolution'
 
     parent_id = fields.Many2one('selection.type', string='Parent')
