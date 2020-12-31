b2b26ef2d08373c21b547d75654f73743494a2de
tme247sur <72996029+tme247sur@users.noreply.github.com>
2020-12-31 09:07:43 -0500

    helpdesk layout

--- custom_module/models/helpdesk.py
+++ custom_module/models/helpdesk.py
@@ -112,7 +112,6 @@ SYSTEM_RELATED_MAIN = [
 class TicketFields(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    multiple_product = fields.Boolean("Multiple Product")
     partner_bus_garage_address = fields.Char(string='Bus Garage Address', compute='_compute_partner_contact', store=True, readonly=False)
     partner_phone = fields.Char(string='Main Phone', compute='_compute_partner_contact', store=True, readonly=False)
     partner_main_contact = fields.Char(string='Primary Contact', compute='_compute_partner_contact', store=True, readonly=False)
@@ -123,8 +122,27 @@ class TicketFields(models.Model):
         for ticket in self:
             if ticket.partner_id:
                 ticket.partner_bus_garage_address = ticket.partner_id.bus_garage_address
-                # ticket.partner_phone = ticket.partner_id.phone
-                # ticket.partner_main_contact = ticket.partner_id.main_contact.partner_id
-                # ticket.partner_main_contact_phone = ticket.partner_id.main_contact_phone
+                ticket.partner_phone = ticket.partner_id.phone
+                ticket.partner_main_contact = ticket.partner_id.main_contact.partner_id
+                ticket.partner_main_contact_phone = ticket.partner_id.main_contact_phone
 
-    
+
+ 
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        if self.partner_id:
+                self.name = partner_id
+                
+            
+
+
+    # @api.onchange('product_main')
+    # def onchange_product_main(self):
+    #     if self.product_main:
+    #         if self.product_issue \
+    #                 and self.product_issue.id not in self.product_main.child_ids.ids:
+    #             self.product_issue = False
+
+
+            
