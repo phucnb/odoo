82d22ba04383a610155b39d04924227c13ccdbb3
tme247sur <72996029+tme247sur@users.noreply.github.com>
2021-01-25 11:34:48 -0500

    Update helpdesk.py

--- custom_module/models/helpdesk.py
+++ custom_module/models/helpdesk.py
@@ -111,27 +111,10 @@ SYSTEM_RELATED_MAIN = [
 
 class TicketFields(models.Model):
     _inherit = 'helpdesk.ticket'
-	# ISSUE TYPE
-	dvr_check = fields.Boolean("DVR")
-	cam_check = fields.Boolean("Camera")
-	storage_check = fields.Boolean("Storage")
-	ap_check = fields.Boolean("Access Point")
-	bridge_check = fields.Boolean("Bridge")
-	td_check = fields.Boolean("Touchdown")
-	dvrv_check = fields.Boolean("DVR Viewer")
-	cust_sys_check = fields.Boolean("Customer Side Issue")
-	# PROD TYPE
-	dvr_type = fields.Selection(DVR_MAIN, "DVR Model")
-	cam_type = fields.Selection(CAMERA_MAIN, "Camera Model")
-	storage_type = fields.Selection(STORAGE_MAIN, "Storage Model")
-	ap_type = fields.Selection(ACCESS_POINT_MAIN, "Access Point Model")
-	bridge_type = fields.Selection(BRIDGE_MAIN, "Bridge Model")
-	td_type = fields.Selection(WIRELESS_MAIN, "Touchdown Type")
-	dvrv_type = fields.Selection(DVR_VIEWER_MAIN, "DVR Viewer Version")
-	customer_sys_type = fields.Selection(SYSTEM_RELATED_MAIN, "Customer System Type")
+
     partner_bus_garage_address = fields.Char(string='Bus Garage Address', compute='_compute_partner_contact', store=True, readonly=False)
-    partner_phone = fields.Char(string='Main Company Phone', compute='_compute_partner_contact', store=True, readonly=False)
-    partner_main_contact = fields.Char(string='Primary Ticket Contact', compute='_compute_partner_contact', store=True, readonly=False)
+    partner_phone = fields.Char(string='Main Phone', compute='_compute_partner_contact', store=True, readonly=False)
+    partner_main_contact = fields.Char(string='Primary Contact', compute='_compute_partner_contact', store=True, readonly=False)
     partner_main_contact_phone = fields.Char(string='Primary Contact Phone', compute='_compute_partner_contact', store=True, readonly=False)
     
 
@@ -143,6 +126,8 @@ class TicketFields(models.Model):
                 ticket.partner_main_contact = ticket.partner_id.main_contact.partner_id
                 ticket.partner_main_contact_phone = ticket.partner_id.main_contact_phone
 
+
+ 
 
     # @api.onchange('partner_id')
     # def onchange_partner_id(self):
