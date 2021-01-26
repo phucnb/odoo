fb27b9d9c570cf2562f9631d3ec87026eb272803
tme247sur <72996029+tme247sur@users.noreply.github.com>
2021-01-26 15:11:36 -0500

    Update helpdesk.py

--- custom_module/models/helpdesk.py
+++ custom_module/models/helpdesk.py
@@ -110,6 +110,10 @@ CUST_SYS_MODEL = [
 class TicketFields(models.Model):
     _inherit = 'helpdesk.ticket'
 
+    partner_bus_garage_address = fields.Char(string='Bus Garage Address', compute='_compute_partner_contact', store=True, readonly=False)
+    partner_phone = fields.Char(string='Main Phone', compute='_compute_partner_contact', store=True, readonly=False)
+    partner_main_contact = fields.Char(string='Primary Contact', compute='_compute_partner_contact', store=True, readonly=False)
+    partner_main_contact_phone = fields.Char(string='Primary Contact Phone', compute='_compute_partner_contact', store=True, readonly=False)
     dvr_check = fields.Boolean("DVR")
     cam_check = fields.Boolean("Camera")
     storage_check = fields.Boolean("Storage")
@@ -127,6 +131,15 @@ class TicketFields(models.Model):
     dvrv_type = fields.Selection(DVRV_VERSION, "DVR Viewer Version")
     cust_sys_type = fields.Selection(CUST_SYS_TYPE, "Customer Related Type")
     dvr_qty_1 = fields.Integer("Quantity")
+
+    def _compute_partner_contact(self):
+        for ticket in self:
+            if ticket.partner_id:
+                ticket.partner_bus_garage_address = ticket.partner_id.bus_garage_address
+                ticket.partner_phone = ticket.partner_id.phone
+                ticket.partner_main_contact = ticket.partner_id.main_contact.partner_id
+                ticket.partner_main_contact_phone = ticket.partner_id.main_contact_phone
+
 
  
 
